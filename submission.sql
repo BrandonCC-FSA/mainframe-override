423661d11f35ab233db7cb6acb55354482ab3117
Brandon Corrigall <corrb812@gmail.com>
2025-06-24 14:59:55 -0400

    forgot to save my finished file

--- submission.sql
+++ submission.sql
@@ -16,14 +16,17 @@ SELECT * FROM emptystack_accounts WHERE last_name = 'Steele';
 
 found an account for Andrew Steele, Username: triple-cart-38, password: password456
 
--- Step 6: Find the message about self-driving taxis and the project code
-SELECT * FROM emptystack_messages WHERE content ILIKE '%self-driving%' OR content ILIKE '%taxi%';
+SELECT * FROM emptystack_messages WHERE body ILIKE '%self-driving%' OR body ILIKE '%taxi%';
 
--- Step 7: Get the admin account credentials (look for the sender of the taxi message)
--- Replace 'ADMIN_USERNAME' with the actual sender username from the message above
-SELECT * FROM emptystack_accounts WHERE username = 'ADMIN_USERNAME';
+found a message sent to user Andrew Steele from username your-boss99
 
--- Step 8: Get the project ID from emptystack_projects
--- Look for project related to taxis or use the project code from the message
-SELECT * FROM emptystack_projects WHERE name ILIKE '%taxi%' OR description ILIKE '%taxi%';
+SELECT * FROM emptystack_accounts WHERE username = 'your-boss-99';
+
+found admin password: notagiancarter for user:your-boss-99
+
+SELECT * FROM emptystack_projects WHERE code = 'TAXI'
+
+found project id: DczE0v2b code: TAXI
+
+
 
